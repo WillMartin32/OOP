2ec2dbd0d111daf6ffeb50d9319edcdf3bec5f23
William Martin <william_martin3@baylor.edu>
2021-09-13 17:21:49 -0500

    Start of class work

---
+++ fly.py
@@ -0,0 +1,11 @@
+import InsectClass as i
+
+mosquito = i.Insect()
+housefly = i.Insect()
+
+mosquito.flight_lenght()
+
+housefly.flight_lenght()
+
+print("the mosquito can fly uo to", mosquito.get_miles(), "miles")
+print("the housefly can fly uo to", housefly.get_miles(), "miles")
