ba36eecd037afaf1b6f93da83b48560778e419dc
William Martin <william_martin3@baylor.edu>
2021-09-20 17:01:26 -0500

    Class work and HW Part1 done

--- OOPHWPart1Class.py
+++ OOPHWPart1Class.py
@@ -1,7 +1,18 @@
 
 
 class RetailItem:
+
     def __init__(self,d,u,p):
         self.__item_description = d
         self.__units_of_inventory = u
         self.__price = p
+
+
+    def get_description(self):
+        return self.__item_description
+
+    def get_units(self):
+        return self.__units_of_inventory
+
+    def get_price(self):
+        return self.__price
--- OOPHWPart1Program.py
+++ OOPHWPart1Program.py
@@ -1,6 +1,20 @@
 import OOPHWPart1Class as c
 
-itemList = {"Item #1":{"Description" : "Jacket", "Units in Inventory" : 12, "Price" : 59.95},
-            "Item #2":{"Description" : "Designer Jeans", "Units in Inventory" : 40, "Price" : 34.95},
-            "Item #3":{"Description" : "Shirt", "Units in Inventory" : 20, "Price" : 24.95}
-}
+
+des1 = 'Jacket'
+unit1 = 12
+price1 = 59.95
+
+Item1 = c.RetailItem(des1,unit1,price1)
+
+des2 = 'Designer Jeans'
+unit2 = 40
+price2 = 34.95
+
+Item2 = c.RetailItem(des2,unit2,price2)
+
+des3 = 'Shirt'
+unit3 = 20
+price3 = 24.95
+
+Item3 = c.RetailItem(des3,unit3,price3)
